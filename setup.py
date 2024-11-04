2b1f9a5659f05d4d62c784e4d9b4bb98d27bb7d8
Yujie Xiang <58551590+xiang2002@users.noreply.github.com>
2024-11-04 14:52:41 +0000

    更新 setup.py 版本号至 0.0.2，并新增 mlClustering 模块初始化文件

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mlClustering",  # 用自己的名替换其中的YOUR_USERNAME_
-    version="0.0.1",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
+    version="0.0.2",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
     author="XiangShan",  # 作者，可以写自己的姓名
     author_email="xiang20020127@gmail.com",  # 作者联系方式，可写自己的邮箱地址
     description="Based on pytorch to implement clustering related algorithms and a little tool",  # 包的简述
